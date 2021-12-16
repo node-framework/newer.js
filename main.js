02ff356057eb895e72206c48369e7cf922f639c7
Revenity <87530663+aquapi@users.noreply.github.com>
2021-12-16 19:27:49 +0700

    Main

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
+const { bodyParser, queryParser, renderHTML } = require("./src/middlewares.mjs");
+
 module.exports = {
     NodeServer: require("./src/nodeserver.mjs").default,
-    Middlewares: require("./src/middlewares.mjs")
+    Middlewares: {
+        bodyParser,
+        queryParser,
+        renderHTML
+    }
 };
