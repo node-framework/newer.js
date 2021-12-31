7c78082f3ce821e983d2e5729275243647c6a5c2
Revenity <87530663+aquapi@users.noreply.github.com>
2021-12-31 08:04:02 +0700

    3.6.0 Alpha

--- doc/example/JsonDb/index.js
+++ doc/example/JsonDb/index.js
@@ -5,19 +5,17 @@ const reviver = new JsonReviver;
 // Set reviver of property accountCreated
 reviver.setReviverOf("accountCreated", e => new Date(e));
 // Database
-const DB = new JsonDB("./example/JsonDB/db/db.json", reviver.callback());
+const DB = new JsonDB("./doc/example/JsonDB/db/db.json", reviver.callback());
 // Print saved item
 DB.on("save-item", console.log);
 // Schema
 const User = DB.schema("User");
-// Create an user
-let user = new User({
+// Create an user and save the document (this action will trigger the save-item event)
+await new User({
     name: "Reve",
     id: 503850,
     accountCreated: new Date(99869369536)
-});
-// Save the document an trigger "save-item"
-await user.save();
+}).save();
 // Find all user
 await User.find({
     name: "Alex"
