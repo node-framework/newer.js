ee4f62ba87d4a5dc34c2a1a9a600644cbc51ffd3
Revenity <87530663+aquapi@users.noreply.github.com>
2022-01-16 23:46:00 +0700

    0.0.x

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,11 @@
-export { default as JsonDB, Schema, SchemaInstance } from "./Database/JsonDB";
-export { default as JsonReviver } from "./Database/JsonReviver";
-export { default as Server, Context, Handler, Middleware, Method } from "./Server/server";  
 export { default as SimpleServer } from "./Server/deno";
+
+import JsonDB, { Schema, SchemaInstance } from "./Database/JsonDB";
+import JsonReviver from "./Database/JsonReviver";
+import Server, { Context, Handler, Middleware, Method } from "./Server/server";  
+
+export default { JsonDB, JsonReviver, Server }
+
+export type {
+    Schema, SchemaInstance, Context, Handler, Method, Middleware
+}
--- types/main.d.ts
+++ types/main.d.ts
@@ -1,4 +1,11 @@
-export { default as JsonDB, Schema, SchemaInstance } from "./Database/JsonDB";
-export { default as JsonReviver } from "./Database/JsonReviver";
-export { default as Server, Context, Handler, Middleware, Method } from "./Server/server";
 export { default as SimpleServer } from "./Server/deno";
+import JsonDB, { Schema, SchemaInstance } from "./Database/JsonDB";
+import JsonReviver from "./Database/JsonReviver";
+import Server, { Context, Handler, Middleware, Method } from "./Server/server";
+declare const _default: {
+    JsonDB: typeof JsonDB;
+    JsonReviver: typeof JsonReviver;
+    Server: typeof Server;
+};
+export default _default;
+export type { Schema, SchemaInstance, Context, Handler, Method, Middleware };
