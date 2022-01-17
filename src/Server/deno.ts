563cf6d40ad4853b531319030e9ba9294aa5ae1e
Revenity <87530663+aquapi@users.noreply.github.com>
2022-01-17 12:05:36 +0700

    0.0.x

--- src/Server/deno.ts
+++ src/Server/deno.ts
@@ -2,18 +2,35 @@ import https from "https";
 import http from "http";
 
 export default class SimpleServer {
-    readonly server: http.Server | https.Server;
+    private server: http.Server | https.Server;
     private done: boolean;
 
+    /**
+     * @param options server options
+     * @param httpsMode toggle HTTPS mode
+     */
     constructor(options?: http.ServerOptions | https.ServerOptions, httpsMode?: boolean) {
         this.server = (httpsMode ? https : http).createServer(options);
     }
 
+    /**
+     * Start the server
+     * 
+     * @param port Server port
+     * @param hostname Server hostname
+     * @param backlog Server backlog
+     * @returns this object
+     */
     listen(port?: number, hostname?: string, backlog?: number) {
         this.server.listen(port, hostname, backlog);
         this.done = false;
+        return this
     }
 
+    /**
+     * Get requests in asynchronous iterator
+     * @returns requests in asynchronous iterator
+     */
     requests() {
         let pointer = this;
         return {
@@ -22,11 +39,15 @@ export default class SimpleServer {
                     async next() {
                         return {
                             done: pointer.done,
-                            value: await new Promise<{request: http.IncomingMessage, response: http.ServerResponse}>(result => {
-                                pointer.server.on('request', (request, response) => 
-                                    result({request, response})
-                                );
-                            })
+                            value: await new Promise<{ request: http.IncomingMessage, response: http.ServerResponse }>(
+                                (result, reject) => {
+                                    pointer.server.on('request', (request, response) =>
+                                        result({ request, response })
+                                    );
+
+                                    pointer.server.on('error', reject);
+                                }
+                            )
                         };
                     }
                 }
@@ -34,8 +55,14 @@ export default class SimpleServer {
         }
     }
 
+    /**
+     * Close the server
+     * 
+     * @returns this object
+     */
     close() {
         this.server.close();
         this.done = true;
+        return this;
     }
 }
--- types/Server/deno.d.ts
+++ types/Server/deno.d.ts
@@ -2,10 +2,26 @@
 import https from "https";
 import http from "http";
 export default class SimpleServer {
-    readonly server: http.Server | https.Server;
+    private server;
     private done;
+    /**
+     * @param options server options
+     * @param httpsMode toggle HTTPS mode
+     */
     constructor(options?: http.ServerOptions | https.ServerOptions, httpsMode?: boolean);
-    listen(port?: number, hostname?: string, backlog?: number): void;
+    /**
+     * Start the server
+     *
+     * @param port Server port
+     * @param hostname Server hostname
+     * @param backlog Server backlog
+     * @returns this object
+     */
+    listen(port?: number, hostname?: string, backlog?: number): this;
+    /**
+     * Get requests in asynchronous iterator
+     * @returns requests in asynchronous iterator
+     */
     requests(): {
         [Symbol.asyncIterator](): {
             next(): Promise<{
@@ -17,5 +33,10 @@ export default class SimpleServer {
             }>;
         };
     };
-    close(): void;
+    /**
+     * Close the server
+     *
+     * @returns this object
+     */
+    close(): this;
 }
