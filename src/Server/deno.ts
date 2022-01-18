d20a7b43c63291ebf00ff7d598a44e30fa516bfd
Revenity <87530663+aquapi@users.noreply.github.com>
2022-01-18 10:14:35 +0700

    0.0.x

--- src/Server/deno.ts
+++ src/Server/deno.ts
@@ -1,6 +1,9 @@
 import https from "https";
 import http from "http";
 
+/**
+ * Server options
+ */
 export interface SimpleOptions {
     /**
      * Server options
@@ -28,32 +31,56 @@ export interface SimpleOptions {
  * Create a simple server
  */
 export default async function* (opts: SimpleOptions = {}) {
+    // Check whether the server has stopped
     let done = false;
-    const server = (opts.httpsMode ? https : http)
+
+    // The server
+    const server = 
+        // Check HTTPS mode
+        (opts.httpsMode ? https : http)
+
+        // Create the server
         .createServer(opts.options)
+
+        // Start the server
         .listen(
             opts.port ?? 80,
             opts.hostname ?? "localhost",
             opts.backlog ?? 0
         )
+
+        // Detect listeners count
         .setMaxListeners(1);
+
+    // Handle each requests using yield
     while (!done)
-        yield new Promise<{ request: http.IncomingMessage, response: http.ServerResponse }>(
-            (result, reject) => {
-                server
-                    // Prevent adding so many listener
-                    .removeAllListeners('request')
-                    .removeAllListeners('error')
-                    // Register event listeners
-                    .on('request',
-                        (request: http.IncomingMessage, response: http.ServerResponse) =>
-                            result({ request, response })
-                    )
-                    .on('error', (err: any) => {
-                        done = true;
-                        reject(err);
-                    });
-            }
+        // Get requests
+        yield new Promise<{
+            /**
+             * The requests
+             */
+            request: http.IncomingMessage,
+            /**
+             * The response
+             */
+            response: http.ServerResponse
+        }>((result, reject) => 
+            server
+                // Remove all previous listener
+                .removeAllListeners('request')
+                .removeAllListeners('error')
+
+                // Register 'request event'
+                .on('request',
+                    (request, response) =>
+                        result({ request, response })
+                )
+
+                // Register 'error' event
+                .on('error', err => {
+                    done = true;
+                    reject(err);
+                })
         );
 };
 
--- types/Server/deno.d.ts
+++ types/Server/deno.d.ts
@@ -1,6 +1,9 @@
 /// <reference types="node" />
 import https from "https";
 import http from "http";
+/**
+ * Server options
+ */
 export interface SimpleOptions {
     /**
      * Server options
@@ -27,6 +30,12 @@ export interface SimpleOptions {
  * Create a simple server
  */
 export default function (opts?: SimpleOptions): AsyncGenerator<{
+    /**
+     * The requests
+     */
     request: http.IncomingMessage;
+    /**
+     * The response
+     */
     response: http.ServerResponse;
 }, void, unknown>;
