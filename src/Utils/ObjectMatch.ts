bdfd5a242b7a316205c1ff1dc661adfa2612f83e
Revenity <87530663+aquapi@users.noreply.github.com>
2022-02-18 13:40:09 +0700

    Error fixing

--- src/Utils/ObjectMatch.ts
+++ src/Utils/ObjectMatch.ts
@@ -1,8 +1,12 @@
 export default function match(obj1: object, obj2: object) {
-    if (obj1 === obj2) 
+    try {
+        if (obj1 === obj2)
+            return true;
+        for (let i in obj1)
+            if (!match(obj1[i], obj2[i]))
+                return false;
         return true;
-    for (let i in obj1)
-        if (!match(obj1[i], obj2[i]))
-            return false;
-    return true;
+    } catch (e) {
+        return false;
+    }
 }
