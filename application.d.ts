7cfbdc2730b792f0543e0da0e9edaf144798bb01
Revenity <87530663+aquapi@users.noreply.github.com>
2022-03-05 23:07:51 +0700

    0.2.x

---
+++ application.d.ts
@@ -0,0 +1,28 @@
+import { AppConfigs } from "./src/declarations";
+
+declare module "newer.js/application" {
+    class Application {
+        /**
+         * Create a new application
+         * @param appConfig the app configuration
+         */
+        constructor(appConfig: AppConfigs);
+
+        /**
+         * Start the app
+         * @returns the http or https server
+         */
+        start(): Promise<import("http").Server | import("https").Server>
+
+        /**
+         * The app configuration
+         */
+        readonly appConfig: AppConfigs;
+
+        /**
+         * Set app configs
+         * @param {import(".").AppConfigs} configs the configs
+         */
+        config(configs: AppConfigs): void;
+    }
+}
--- index.d.ts
+++ index.d.ts
@@ -5,47 +5,16 @@ import simple from "./src/Server/simple";
 import SubDomain from "./src/Middleware/subdomain";
 import Cookie from "./src/Middleware/cookie";
 import JsonDB from "./src/Database/JsonDB";
-import { AppConfigs } from "./src/declarations";
 
-declare module "newer.js" {
-    export {
-        Server,
-        Router,
-        StaticDir,
-        simple,
-        SubDomain,
-        Cookie,
-        JsonDB,
-    }
-
-    export * from "./src/declarations";
+export {
+    Server,
+    Router,
+    StaticDir,
+    simple,
+    SubDomain,
+    Cookie,
+    JsonDB,
 }
 
-declare module "newer.js/application" {
-    class Application {
-        /**
-         * Create a new application
-         * @param appConfig the app configuration
-         */
-        constructor(appConfig: AppConfigs);
+export * from "./src/declarations";
 
-        /**
-         * Start the app
-         * @returns the http or https server
-         */
-        start(): Promise<import("http").Server | import("https").Server>
-
-        /**
-         * The app configuration
-         */
-        readonly appConfig: AppConfigs;
-
-        /**
-         * Set app configs
-         * @param {import(".").AppConfigs} configs the configs
-         */
-        config(configs: AppConfigs): void;
-    }
-
-    export = Application;
-}
