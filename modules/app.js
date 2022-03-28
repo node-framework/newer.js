61a9faad70b70afe4059209cdc07e1665542a565
Revenity <87530663+aquapi@users.noreply.github.com>
2022-03-28 21:30:06 +0700

    0.2.x

--- modules/app.js
+++ modules/app.js
@@ -1,6 +1,5 @@
 // @ts-check
 
-const Router = require("../lib/Middleware/router.js").default;
 const Server = require("../lib/Server/server.js").default;
 const StaticDir = require("../lib/Middleware/staticdir.js").default;
 const { readdirSync, existsSync } = require("fs");
