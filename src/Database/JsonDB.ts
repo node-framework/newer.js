a38021005c30815b71208fac45973782048c185e
Revenity <87530663+aquapi@users.noreply.github.com>
2022-04-02 12:47:30 +0700

    0.2.x

--- src/Database/JsonDB.ts
+++ src/Database/JsonDB.ts
@@ -8,7 +8,7 @@ import match from "../Utils/ObjectMatch";
 import drop from "../Utils/Compiler/drop";
 import get from "../Utils/Compiler/get";
 import rmDuplicates from "../Utils/RmDuplicates";
-import forEachParallel from "../Utils/ForEachParallel";
+import asyncUtil from "async";
 
 // Email regex
 const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
@@ -189,21 +189,28 @@ export default class JsonDB {
                 return pfs.writeFile(ptr.path, JSON.stringify(ptr.cache));
             }
 
-            // TODO: async find
+            // Async find
             static async find(obj?: any, count?: number, except?: boolean) {
-                const result = [];
-                
+                let resCount: number = 0;
+
                 // Loop parallel
-                await forEachParallel(ptr.cache[name], item => {
+                return asyncUtil.filter(ptr.cache[name], (item, cb) => {
                     // Check whether the result is enough
-                    if (count && result.length >= count)
+                    if (count && resCount >= count) {
+                        cb(null, false);
                         return;
+                    }
 
                     // Add the object to the result if matches
-                    if (match(obj, item) === !except)
-                        result.push(item);
+                    const condition = match(obj, item) === !except;
+
+                    // Check the condition
+                    if (condition)
+                        resCount++;
+
+                    // End
+                    cb(null, condition);
                 });
-                return result;
             }
 
             // Find one
--- src/Utils/ForEachParallel.ts
+++
@@ -1,9 +0,0 @@
-/**
- * For each parallel
- */
-export default async<T>(arr: T[], func: (item: T) => Promise<void> | void) => 
-    Promise.all(
-        arr.map(
-            async (item: T) => func(item)
-        )
-    );
