a2128c1f0b500c7cc8af3f593add5bb71a1b9844
Revenity <87530663+aquapi@users.noreply.github.com>
2022-04-16 23:33:14 +0700

    Fix BodyParser

--- src/Utils/BodyParser.ts
+++ src/Utils/BodyParser.ts
@@ -1,4 +1,5 @@
 import http from "http";
+import formidable from "formidable";
 
 // Try parse to JSON
 function tryParseJSON(body: string): object {
@@ -9,23 +10,42 @@ function tryParseJSON(body: string): object {
     }
 }
 
+// Try parse form
+async function tryParseForm(req: http.IncomingMessage) {
+    const form = formidable({
+        keepExtensions: true
+    });
+    return new Promise((res, rej) =>
+        form.parse(req, (err, fields, files) => {
+            if (err)
+                return rej(err);
+            res({ fields, files });
+        })
+    );
+}
+
 // Try parse to URLSearchParams
 function tryParseQuery(body: string): { [key: string]: string } {
-    if (!body) 
+    if (!body)
         return;
     const result: { [key: string]: string } = {};
     for (const key of body.split("&")) {
         if (!key)
             continue;
         const [k = key, v = ""] = key.split("=");
-        result[k] = v;
+        result[decodeURIComponent(k.replaceAll("%20", " "))] = decodeURIComponent(v.replaceAll("%20", " "));
     }
     return result;
 }
 
 // Get the body of a request
-export const getBody = async (req: http.IncomingMessage): Promise<any> =>
-    new Promise((res, rej) => {
+export const getBody = async (req: http.IncomingMessage): Promise<any> => {
+    // Special case for form
+    if (req.headers['content-type']?.startsWith('multipart/form-data'))
+        return tryParseForm(req);
+
+    // JSON and query body
+    return new Promise((res, rej) => {
         let body = '';
         req.on('data', data => {
             body += data;
@@ -35,13 +55,18 @@ export const getBody = async (req: http.IncomingMessage): Promise<any> =>
             }
         });
         req.on('end', () => {
-            const parsed =
-                tryParseJSON(body)
-                ?? tryParseQuery(body)
-                ?? body;
+            let parsed: any = body;
+            if (req.headers['content-type']) {
+                // Parse by content type
+                if (req.headers['content-type'].startsWith('application/json'))
+                    parsed = tryParseJSON(body);
+                else if (req.headers['content-type'].startsWith('application/x-www-form-urlencoded'))
+                    parsed = tryParseQuery(body);
+            }
             res(parsed);
         });
     });
+}
 
 // Get query of an URL
 export const getQuery = (url: string): { [key: string]: string } => {
